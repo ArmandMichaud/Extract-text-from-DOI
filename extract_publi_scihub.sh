ed5a6de7472963a4462aa3e4bc316521fe2de9c7
ArmandMichaud <117903627+ArmandMichaud@users.noreply.github.com>
2025-01-27 16:33:09 +0100

    Update extract_publi_scihub.sh

--- extract_publi_scihub.sh
+++ extract_publi_scihub.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#This bash script is used to download PDF files corresponding to DOIs listed in a text file. 
-#It checks if the necessary argument is passed and verifies the existence of the input file. 
-#Then, it iterates through each DOI, downloads the corresponding page from Scihub, extracts the URL of the PDF file using a Python script, and downloads the PDF file. 
-#If any error occurs during this process, it logs the DOI into an error file. 
-#Finally, it prints a message indicating the completion of the download process.
-
+# This bash script is used to download PDF files corresponding to DOIs listed in a text file.
+# It checks if the necessary argument is passed and verifies the existence of the input file.
+# Then, it iterates through each DOI, downloads the corresponding page from Scihub, extracts the URL of the PDF file using a Python script, and downloads the PDF file.
+# If any error occurs during this process, it logs the DOI into an error file.
+# Finally, it checks for 0-byte PDF files, deletes them, and logs the corresponding DOIs.
 
 mkdir -p data_pdf
 mkdir -p logs
@@ -25,7 +24,6 @@ if [ ! -f "$file" ]; then
   exit 1
 fi
 
-
 # Initialize the publication number
 publication_num=1
 
@@ -33,7 +31,7 @@ publication_num=1
 while IFS= read -r doi; do
 
   # Download the corresponding page
-  wget -O temppage "https://sci-hub.3800808.com/$doi"
+  wget -O temppage --header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" "https://sci-hub.3800808.com/$doi"
 
   # Execute the Python script to extract the URL
   url=$(python3 script_python/extract_publi.py temppage)
@@ -46,7 +44,6 @@ while IFS= read -r doi; do
   fi
 
   # Modify the DOI
-  #modified_doi="${doi//\//%2F}"
   modified_doi=$(echo "$doi" | sed -e 's/</%3C/g' -e 's/>/%3E/g' -e 's/:/%3A/g' -e 's/;/%3B/g' -e 's/\//%2F/g')
 
   # Download the PDF file
@@ -61,5 +58,23 @@ while IFS= read -r doi; do
 
 done < "$file"
 
-echo "**PDF file download completed. Files are stored in the data_pdf folder.**"
+# Check for 0-byte files and log DOIs for deletion
+for pdf_file in data_pdf/*.pdf; do
+  if [ ! -s "$pdf_file" ]; then
+    # Convert the filename back to the DOI
+    base_name=$(basename "$pdf_file" .pdf)
+    original_doi=$(echo "$base_name" | sed -e 's/%3C/</g' -e 's/%3E/>/g' -e 's/%3A/:/g' -e 's/%3B/;/g' -e 's/%2F/\//g')
 
+    # Log the DOI
+    echo "$original_doi" >> ./logs/errors_extract_scihub.txt
+
+    # Delete the empty file
+    rm "$pdf_file"
+  fi
+
+done
+
+echo "**PDF file download completed. Files are stored in the data_pdf folder.**"
+if [ -f temppage ] ; then
+    rm temppage
+fi
